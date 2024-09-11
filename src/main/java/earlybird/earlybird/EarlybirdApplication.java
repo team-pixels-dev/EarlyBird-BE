172e8211344b0cdcf5cade7e6ced3b322ec84d79
Yun-YeoJun <yyj8771@naver.com>
2024-09-11 20:57:43 +0900

    Initial commit

---
+++ src/main/java/earlybird/earlybird/EarlybirdApplication.java
@@ -0,0 +1,13 @@
+package earlybird.earlybird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarlybirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarlybirdApplication.class, args);
+	}
+
+}
