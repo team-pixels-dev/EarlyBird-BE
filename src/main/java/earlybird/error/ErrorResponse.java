97b15a9df5d6562ae66f0af0ca314d8b15d0fe23
HyoJeong Kang <87938438+comom87@users.noreply.github.com>
2024-09-24 15:26:20 +0900

    [Feature] 예외 처리 (#8)
    * feat: ErrorCode 정의
    
    * feat: ErrorResponse 형식 구현
    
    * feat: GlobalExceptionHandler 구현
    
    * feat: Exception 구현
    
    * fix: printStackTrace 삭제
    
    * fix: 추상 클래스로 변경

---
+++ src/main/java/earlybird/error/ErrorResponse.java
@@ -0,0 +1,31 @@
+package earlybird.error;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ErrorResponse {
+    private HttpStatus status;
+    private String message;
+
+    private ErrorResponse(final ErrorCode errorCode) {
+        this.status = errorCode.getStatus();
+        this.message = errorCode.getMessage();
+    }
+
+    public ErrorResponse(final ErrorCode errorCode, final String message) {
+        this.status = errorCode.getStatus();
+        this.message = errorCode.getMessage();
+    }
+
+    public static ErrorResponse of(final ErrorCode errorCode) {
+        return new ErrorResponse(errorCode);
+    }
+
+    public static ErrorResponse of(final ErrorCode errorCode, final String message) {
+        return new ErrorResponse(errorCode, message);
+    }
+}
---
+++ src/main/java/earlybird/error/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package earlybird.error;
+
+import earlybird.error.exception.BusinessBaseException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        log.error("HttpRequestMethodNotSupportedException", e);
+        return createErrorResponseEntity(ErrorCode.METHOD_NOT_ALLOWED);
+    }
+
+    @ExceptionHandler(BusinessBaseException.class)
+    protected ResponseEntity<ErrorResponse> handleBusinessBaseException(BusinessBaseException e) {
+        log.error("BusinessException", e);
+        return createErrorResponseEntity(e.getErrorCode());
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("Exception", e);
+        return createErrorResponseEntity(ErrorCode.INTERNAL_SERVER_ERRER);
+    }
+
+    private ResponseEntity<ErrorResponse> createErrorResponseEntity(ErrorCode errorCode) {
+        return new ResponseEntity<>(ErrorResponse.of(errorCode), errorCode.getStatus());
+    }
+}
