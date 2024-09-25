9c94dfc1534ee9049a7fc8f477b491cd843ae5a5
Yun-YeoJun <yyj8771@naver.com>
2024-09-25 15:35:25 +0900

    [refactor] OAuth2ProviderName Enum 도입

--- src/main/java/earlybird/earlybird/security/authentication/oauth2/OAuth2AuthenticationProvider.java
+++ src/main/java/earlybird/earlybird/security/authentication/oauth2/OAuth2AuthenticationProvider.java
@@ -4,6 +4,7 @@ import earlybird.earlybird.security.authentication.oauth2.dto.OAuth2ServerRespon
 import earlybird.earlybird.security.authentication.oauth2.proxy.GoogleOAuth2UserInfoProxy;
 import earlybird.earlybird.security.authentication.oauth2.proxy.OAuth2UserInfoProxy;
 import earlybird.earlybird.security.authentication.oauth2.user.OAuth2UserJoinService;
+import earlybird.earlybird.security.enums.OAuth2ProviderName;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.AuthenticationServiceException;
@@ -13,13 +14,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
 public class OAuth2AuthenticationProvider implements AuthenticationProvider {
 
-    private final static Map<String, OAuth2UserInfoProxy> oauth2UserInfoProxyList = Map.of("google", new GoogleOAuth2UserInfoProxy());
+    private final static Map<OAuth2ProviderName, OAuth2UserInfoProxy> oauth2UserInfoProxyList
+            = Map.of(OAuth2ProviderName.GOOGLE, new GoogleOAuth2UserInfoProxy());
 
     private final UserDetailsService userDetailsService;
     private final OAuth2UserJoinService oAuth2UserJoinService;
@@ -29,24 +30,28 @@ public class OAuth2AuthenticationProvider implements AuthenticationProvider {
         String oauth2ProviderName = ((String) authentication.getPrincipal()).toLowerCase();
         String oauth2AccessToken = (String) authentication.getCredentials();
 
-        if (!oauth2UserInfoProxyList.containsKey(oauth2ProviderName)) {
-            throw new AuthenticationServiceException("지원하지 않는 OAuth2 provider입니다.");
+
+        for (OAuth2ProviderName oAuth2ProviderName : oauth2UserInfoProxyList.keySet()) {
+            String proxyName = oAuth2ProviderName.name();
+            if (proxyName.equalsIgnoreCase(oauth2ProviderName)) {
+                OAuth2UserInfoProxy oAuth2UserInfoProxy = oauth2UserInfoProxyList.get(oAuth2ProviderName);
+                OAuth2ServerResponse oAuth2UserInfo = oAuth2UserInfoProxy.getOAuth2UserInfo(oauth2AccessToken);
+
+                String username = oAuth2UserInfo.getProviderName() + " " + oAuth2UserInfo.getProviderId();
+
+                UserDetails userDetails;
+                try {
+                    userDetails = userDetailsService.loadUserByUsername(username);
+                } catch (UsernameNotFoundException e) {
+                    oAuth2UserJoinService.join(oAuth2UserInfo);
+                    userDetails = userDetailsService.loadUserByUsername(username);
+                }
+
+                return new OAuth2AuthenticationToken(userDetails.getAuthorities(), userDetails, null);
+            }
         }
 
-        OAuth2UserInfoProxy oAuth2UserInfoProxy = oauth2UserInfoProxyList.get(oauth2ProviderName);
-        OAuth2ServerResponse oAuth2UserInfo = oAuth2UserInfoProxy.getOAuth2UserInfo(oauth2AccessToken);
-
-        String username = oAuth2UserInfo.getProviderName() + " " + oAuth2UserInfo.getProviderId();
-
-        UserDetails userDetails;
-        try {
-            userDetails = userDetailsService.loadUserByUsername(username);
-        } catch (UsernameNotFoundException e) {
-            oAuth2UserJoinService.join(oAuth2UserInfo);
-            userDetails = userDetailsService.loadUserByUsername(username);
-        }
-
-        return new OAuth2AuthenticationToken(userDetails.getAuthorities(), userDetails, null);
+        throw new AuthenticationServiceException("지원하지 않는 OAuth2 provider입니다.");
     }
 
     @Override
---
+++ src/main/java/earlybird/earlybird/security/enums/OAuth2ProviderName.java
@@ -0,0 +1,5 @@
+package earlybird.earlybird.security.enums;
+
+public enum OAuth2ProviderName {
+    GOOGLE, APPLE
+}
