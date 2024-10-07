6e014a4d61d2f8e99501f78a194495ae558dce6d
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2024-10-07 12:48:43 +0900

    [refactor] FeedbackRepository를 repository 폴더 안으로 이동

--- src/main/java/earlybird/earlybird/feedback/repository/FeedbackRepository.java
+++ src/main/java/earlybird/earlybird/feedback/repository/FeedbackRepository.java
@@ -1,5 +1,6 @@
-package earlybird.earlybird.feedback;
+package earlybird.earlybird.feedback.repository;
 
+import earlybird.earlybird.feedback.entity.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
