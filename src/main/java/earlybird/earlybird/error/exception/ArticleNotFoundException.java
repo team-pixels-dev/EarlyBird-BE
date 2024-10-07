28f2e7429e027c9b1bc5a2a925db4625618b9938
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2024-10-07 19:40:06 +0900

    Revert "Fix - error 패키지 경로 수정"

--- src/main/java/earlybird/earlybird/error/exception/ArticleNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package earlybird.earlybird.error.exception;
-
-import earlybird.earlybird.error.ErrorCode;
-
-public class ArticleNotFoundException extends NotFoundException {
-    public ArticleNotFoundException() {
-        super(ErrorCode.ARTICLE_NOT_FOUND);
-    }
-}
