9e491b071fe903efaed38a9b18285408bc9aacfa
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2025-02-27 03:22:46 +0900

    [test] 예외 처리와 로그 관련 테스트 코드 작성

---
+++ src/test/java/earlybird/earlybird/error/GlobalExceptionHandlerUnitTest.java
@@ -0,0 +1,145 @@
+package earlybird.earlybird.error;
+
+import earlybird.earlybird.EarlybirdApplication;
+import earlybird.earlybird.error.exception.BusinessBaseException;
+import earlybird.earlybird.error.exception.NotFoundException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.lang.reflect.Constructor;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(OutputCaptureExtension.class)
+class GlobalExceptionHandlerUnitTest {
+
+    private final String requestURI = "/exception-uri";
+
+    @DisplayName("Exception.class 핸들러 메시지 검증")
+    @Test
+    void handleExceptionMessage(CapturedOutput output) throws Exception {
+        Exception exception = initException(Exception.class);
+        MockHttpServletRequest request = initRequest();
+
+        GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
+
+        globalExceptionHandler.handleException(exception, request);
+
+        assertThat(output.getOut()).contains("Exception for " + requestURI + ": " + exception.getMessage());
+        assertThat(output.getOut()).contains("ERROR");
+    }
+
+    @DisplayName("HttpRequestMethodNotSupportedException.class 핸들러 메시지 검증")
+    @Test
+    void handleHttpRequestMethodNotSupportedExceptionMessage(CapturedOutput output) throws Exception {
+        HttpRequestMethodNotSupportedException exception = (HttpRequestMethodNotSupportedException) initException(HttpRequestMethodNotSupportedException.class);
+        MockHttpServletRequest request = initRequest();
+
+        GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
+
+        globalExceptionHandler.handleHttpRequestMethodNotSupportedException(exception, request);
+
+        assertThat(output.getOut()).contains("HttpRequestMethodNotSupportedException for " + requestURI + ": " + exception.getMessage());
+        assertThat(output.getOut()).contains("WARN");
+    }
+
+    @DisplayName("BusinessBaseException.class 핸들러 메시지 검증")
+    @Test
+    void handleBusinessBaseExceptionMessage(CapturedOutput output) throws Exception {
+        BusinessBaseException exception = (BusinessBaseException) initException(BusinessBaseException.class);
+        MockHttpServletRequest request = initRequest();
+
+        GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
+
+        globalExceptionHandler.handleBusinessBaseException(exception, request);
+
+        assertThat(output.getOut()).contains("BusinessException for " + requestURI + ": " + exception.getMessage());
+        assertThat(output.getOut()).contains("WARN");
+    }
+
+    @DisplayName("NoResourceFoundException.class 핸들러 메시지 검증")
+    @Test
+    void handleNoResourceFoundExceptionMessage(CapturedOutput output) throws Exception {
+        NoResourceFoundException exception = (NoResourceFoundException) initException(NoResourceFoundException.class);
+        MockHttpServletRequest request = initRequest();
+
+        GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
+
+        globalExceptionHandler.handleNoResourceFoundException(exception, request);
+
+        assertThat(output.getOut()).contains("NoResourceFoundException for " + requestURI + ": " + exception.getMessage());
+        assertThat(output.getOut()).contains("WARN");
+    }
+
+    @DisplayName("HttpMessageNotReadableException.class 핸들러 메시지 검증")
+    @Test
+    void handleHttpMessageNotReadableExceptionMessage(CapturedOutput output) throws Exception {
+        HttpMessageNotReadableException exception = (HttpMessageNotReadableException) initException(HttpMessageNotReadableException.class);
+        MockHttpServletRequest request = initRequest();
+
+        GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
+
+        globalExceptionHandler.handleHttpMessageNotReadableException(exception, request);
+
+        assertThat(output.getOut()).contains("HttpMessageNotReadableException for " + requestURI + ": " + exception.getMessage());
+        assertThat(output.getOut()).contains("WARN");
+    }
+
+    @DisplayName("MethodArgumentNotValidException.class 핸들러 메시지 검증")
+    @Test
+    void handleMethodArgumentNotValidExceptionMessage(CapturedOutput output) throws Exception {
+        MethodArgumentNotValidException exception = (MethodArgumentNotValidException) initException(MethodArgumentNotValidException.class);
+        MockHttpServletRequest request = initRequest();
+
+        GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
+
+        globalExceptionHandler.handleMethodArgumentNotValidException(exception, request);
+
+        assertThat(output.getOut()).contains("MethodArgumentNotValidException for " + requestURI + ": " + exception.getMessage());
+        assertThat(output.getOut()).contains("WARN");
+    }
+
+    private Exception initException(Class<? extends Exception> exceptionClass) throws Exception {
+        String exceptionMessage = "exception-message";
+
+        Constructor<? extends Exception> constructor;
+        Exception exception;
+
+        if (exceptionClass.isAssignableFrom(BusinessBaseException.class)) {
+            exception = new NotFoundException();
+        }
+        else if (exceptionClass.isAssignableFrom(NoResourceFoundException.class)) {
+            exception = new NoResourceFoundException(HttpMethod.GET, "resource-path");
+        }
+        else if (exceptionClass.isAssignableFrom(MethodArgumentNotValidException.class)) {
+            exception = new MethodArgumentNotValidException(
+                    new MethodParameter(EarlybirdApplication.class.getMethod("main", String[].class), 0),
+                    new BeanPropertyBindingResult(null, "param"));
+        }
+        else {
+            constructor = exceptionClass.getConstructor(String.class);
+            exception = constructor.newInstance(exceptionMessage);
+        }
+
+        return exception;
+    }
+
+    private MockHttpServletRequest initRequest() {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setRequestURI(requestURI);
+        return request;
+    }
+}
---
+++ src/test/java/earlybird/earlybird/log/MdcLogTraceFilterTest.java
@@ -0,0 +1,58 @@
+package earlybird.earlybird.log;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.client.response.MockRestResponseCreators;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(OutputCaptureExtension.class)
+@ExtendWith(SpringExtension.class)
+@TestPropertySource(properties = {
+        "aws.access-key=access-key",
+        "aws.secret-access-key=secret-key",
+        "spring.jwt.secret=jwt-secret",
+        "fcm.project-id=project-id",
+        "spring.profiles.active=test"
+})
+class MdcLogTraceFilterTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @DisplayName("모든 요청의 각 로그에는 요청마다 고유한 trace id가 기록된다")
+    @Test
+    void traceId(CapturedOutput output) throws Exception {
+        // given
+        mockMvc.perform(MockMvcRequestBuilders.get("/not-found-uri"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+
+        assertThat(output.getOut()).contains("\"trace-id\":");
+    }
+
+    @DisplayName("모든 HTTP 요청의 각 로그에는 요청 URI 가 기록된다")
+    @Test
+    void requestUri(CapturedOutput output) throws Exception {
+        // given
+        mockMvc.perform(MockMvcRequestBuilders.get("/not-found-uri"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+
+        assertThat(output.getOut()).contains("\"request-uri\":\"/not-found-uri\"");
+    }
+
+}
