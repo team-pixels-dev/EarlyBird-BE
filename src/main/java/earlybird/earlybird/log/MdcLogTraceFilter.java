c1ecc3ae7b8461bd37f164509ecfec244617d956
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2025-02-28 23:42:03 +0900

    [feature] 모든 로그에 요청 IP가 기록되도록 설정

--- src/main/java/earlybird/earlybird/log/MdcLogTraceFilter.java
+++ src/main/java/earlybird/earlybird/log/MdcLogTraceFilter.java
@@ -2,9 +2,7 @@ package earlybird.earlybird.log;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
-
 import lombok.extern.slf4j.Slf4j;
-
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +14,7 @@ import java.util.UUID;
 public class MdcLogTraceFilter implements Filter {
     private static final String TRACE_ID_KEY = "trace-id";
     private static final String REQUEST_URI_KEY = "request-uri";
+    private static final String REQUEST_IP_KEY = "request-ip";
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -25,11 +24,18 @@ public class MdcLogTraceFilter implements Filter {
 
             if (request instanceof HttpServletRequest httpRequest) {
                 MDC.put(REQUEST_URI_KEY, httpRequest.getRequestURI());
+                MDC.put(REQUEST_IP_KEY, getRequestIp(httpRequest));
             }
 
             chain.doFilter(request, response);
         } finally {
             MDC.clear();
         }
+    }
+
+    private String getRequestIp(HttpServletRequest httpRequest) {
+        String requestIp = httpRequest.getHeader("X-Forwarded-For");
+        if (requestIp == null) requestIp = httpRequest.getRemoteAddr();
+        return requestIp;
     }
 }
