f58f7b7bb50e47741351fe31da01630a4c24e096
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2025-02-28 22:10:09 +0900

    [fix] 로깅 유틸 클래스 리팩토링에 맞춰 수정

--- src/main/java/earlybird/earlybird/log/visit/service/VisitEventLogServiceV2.java
+++ src/main/java/earlybird/earlybird/log/visit/service/VisitEventLogServiceV2.java
@@ -1,6 +1,6 @@
 package earlybird.earlybird.log.visit.service;
 
-import earlybird.earlybird.log.util.LogUtil;
+import earlybird.earlybird.common.util.LogUtil;
 import earlybird.earlybird.log.visit.service.request.VisitEventLoggingServiceRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Primary;
@@ -8,15 +8,16 @@ import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
+import static org.springframework.boot.logging.LogLevel.INFO;
+
 @Slf4j
 @Primary
 @Service
 public class VisitEventLogServiceV2 implements VisitEventLogService {
     @Override
     public void create(VisitEventLoggingServiceRequest request) {
-        LogUtil.infoLog(
-                Map.of("client-id", request.getClientId(), "event-type", "client-visit"),
+        LogUtil.log(
+                INFO, Map.of("client-id", request.getClientId(), "event-type", "client-visit"),
                 "visit log: client-id={}", request.getClientId());
-
     }
 }
