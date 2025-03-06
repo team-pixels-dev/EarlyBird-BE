eb61c9bc0022baf67e2b1e261719c1b0526db65a
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2025-03-06 20:26:56 +0900

    [fix] CascadeType.PERSIST 옵션 추가

--- src/main/java/earlybird/earlybird/feedback/domain/score/FeedbackScoreDayInfo.java
+++ src/main/java/earlybird/earlybird/feedback/domain/score/FeedbackScoreDayInfo.java
@@ -24,7 +24,7 @@ public class FeedbackScoreDayInfo extends BaseTimeEntity {
 
     private Integer day;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "feedback_score_id")
     private FeedbackScore feedbackScore;
 }
