0415d53ada4ff16e3be6a28ff0f76f90075d9a98
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2025-08-08 13:26:49 +0900

    [temp] 모든 요청 임시 허용

--- src/main/java/earlybird/earlybird/security/config/SecurityConfig.java
+++ src/main/java/earlybird/earlybird/security/config/SecurityConfig.java
@@ -22,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -53,6 +54,7 @@ public class SecurityConfig {
     private final SaveJWTRefreshTokenService saveJWTRefreshTokenService;
     private final OAuth2AuthenticationProvider oAuth2AuthenticationProvider;
 
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         AuthenticationManager authenticationManager = authenticationManager(http);
@@ -60,10 +62,9 @@ public class SecurityConfig {
 
         http.authorizeHttpRequests(
                 auth ->
-                        auth.requestMatchers("/api/v1/login/oauth2")
-                                .permitAll()
-                                .anyRequest()
-                                .authenticated());
+//                        auth.requestMatchers("/api/v1/login/oauth2").permitAll()
+//                                .anyRequest().authenticated());
+                        auth.anyRequest().permitAll());
 
         OAuth2AuthenticationFilter oAuth2AuthenticationFilter = oAuth2AuthenticationFilter();
         oAuth2AuthenticationFilter.setAuthenticationManager(authenticationManager);
